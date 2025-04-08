408f6797dc372b38a44987497d7f96a8d803349f
xtoazt <mynameisrohanandthisismyemail@gmail.com>
2025-04-08 09:31:40 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,20 +1,21 @@
-importScripts('/coffee/uv.bundle.js')
-importScripts('/coffee/uv.config.js')
-importScripts(__uv$config.sw || '/coffee/uv.sw.js')
+var cacheName = 'TIWcog';
+var filesToCache = [
+  '/js/sw.js'
+];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(self.skipWaiting())
-})
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+  self.skipWaiting();
+});
 
-const sw = new UVServiceWorker()
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    (async () => {
-      if (event.request.url.startsWith(location.origin + __uv$config.prefix)) {
-        return await sw.fetch(event)
-      }
-      return await fetch(event.request)
-    })()
-  )
-})
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
