404a01a0ffa2b1d24165f48c0c4c1a9081c4c7c2
xtoazt <mynameisrohanandthisismyemail@gmail.com>
2025-04-08 09:32:57 -0400

    Update uv.config.js

--- public/coffee/uv.config.js
+++ public/coffee/uv.config.js
@@ -1,11 +1,13 @@
-/*global Ultraviolet*/
 self.__uv$config = {
-  prefix: '/~/',
-  encodeUrl: Ultraviolet.codec.xor.encode,
-  decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: '/coffee/uv.handler.js',
-  client: '/coffee/uv.client.js',
-  bundle: '/coffee/uv.bundle.js',
-  config: '/coffee/uv.config.js',
-  sw: '/coffee/uv.sw.js'
-}
+    prefix: '/embed/',
+    
+    // BARE SERVER
+    bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
+    
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/coffee/uv.handler.js',
+    bundle: '/coffee/uv.bundle.js',
+    config: '/coffee/uv.config.js',
+    sw: '/coffee/uv.sw.js',
+};
